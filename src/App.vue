945a7ad0417dff42043f2bf73308f1fb920cbb37
LuizGabriel2000 <luizgabriel.lg2000@outlook.com>
2022-08-23 10:19:58 -0300

    implementada a bliblioteca bootstrap

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <HomeUniverso></HomeUniverso>
+</template>
+
+<script>
+import HomeUniverso from './views/HomeUniverso.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    HomeUniverso
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
